7f1ef2dd609e76a000543642e82e84febd4f8eb6
Aaron Yates <aaronmyates@hotmail.com>
2021-04-18 06:51:16 -0400

    refactoring for material-ui framework. Finished basic landing page skeleton.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import './App.css';
+import { GlobalStyles } from "./theme/GlobalStyle";
 import { Switch, Route } from 'react-router-dom';
-import GlobalStyle from "./theme/GlobalStyle";
-import LandingPage from "./components/LandingPage.js";
-import Register from "./components/Register.js";
-import Login from "./components/Login.js";
-import Search from "./components/Search";
+import Landing from "./components/pages/Landing";
+
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
-
+      <GlobalStyles />
       <Switch>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
+        
         <Route path="/">
-          <LandingPage />
+          <Landing />
         </Route>
       </Switch>
     </div>
